38e0e4fb3b0dfd4a9c4d46eb5c3193686bc52003
kelter-js <spawnkillerqw@gmail.com>
2024-04-05 20:56:11 +0500

    feat: updated Task Form

--- components/TaskForm/TaskForm.tsx
+++ components/TaskForm/TaskForm.tsx
@@ -1,17 +1,31 @@
 import { Pressable, Text, TouchableOpacity, StyleSheet } from "react-native";
-import { FC, useCallback, useState } from "react";
+import { FC, useCallback, useEffect, useState } from "react";
 
-import { TaskFormProps } from "../../view";
+import { TaskFormProps } from "../../models";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 import DatePicker from "../DatePicker";
+import DateDisplay from "../DateDisplay";
 
 const removeButtonDescription = (isViewModeInProgress: boolean) =>
-  isViewModeInProgress ? "Mark as done" : "Delete";
+  isViewModeInProgress ? (
+    <>
+      Done
+      <Ionicons name="checkmark-done-circle" size={24} color="green" />
+    </>
+  ) : (
+    <>
+      Delete
+      <MaterialIcons name="delete-sweep" size={24} color="red" />
+    </>
+  );
 
 //place buttons near bottom of modal
-//replace delete button with trash icon
-//replace mark as done/mark as unfinished with icons(think about it)
+//rearrange layout of buttons placement
+//we should display selected date in a more understandable way
 //create accent on which date to remind is settled
-//create edit view mode, like pencil icon, user click - we close this modal
 const TaskForm: FC<TaskFormProps> = ({
   onRemoveTask,
   onMoveTaskBack,
@@ -30,20 +44,31 @@ const TaskForm: FC<TaskFormProps> = ({
   return (
     <>
       <Pressable
-        style={[styles.button, styles.buttonClose]}
+        style={[
+          styles.button,
+          styles.actionButton,
+          isViewModeInProgress ? styles.doneButton : styles.deleteButton,
+        ]}
         onPress={onRemoveTask}
       >
-        <Text style={styles.textStyle}>
+        <Text style={styles.actionText}>
           {removeButtonDescription(isViewModeInProgress)}
         </Text>
       </Pressable>
 
       {!isViewModeInProgress && (
         <Pressable
-          style={[styles.button, styles.buttonClose]}
+          style={[styles.button, styles.actionButton]}
           onPress={onMoveTaskBack}
         >
-          <Text style={styles.textStyle}>Mark as unfinished task</Text>
+          <Text style={styles.actionText}>
+            Unfinished task
+            <MaterialCommunityIcons
+              name="progress-check"
+              size={24}
+              color="black"
+            />
+          </Text>
         </Pressable>
       )}
 
@@ -52,18 +77,22 @@ const TaskForm: FC<TaskFormProps> = ({
           <Pressable
             style={[
               styles.button,
-              styles.buttonClose,
+              styles.actionButton,
               !isDateSelected && styles.buttonDisabled,
             ]}
             disabled={!isDateSelected}
             onPress={() => onCreateReminder(selectedDate)}
           >
-            <Text style={styles.textStyle}>
-              Remind me about this task later
+            <Text style={styles.actionText}>
+              Remind later
+              <Entypo name="back-in-time" size={24} color="black" />
             </Text>
           </Pressable>
         </TouchableOpacity>
       )}
+
+      {isViewModeInProgress && <div>Selected date is:</div>}
+      {isViewModeInProgress && <DateDisplay date={selectedDate} />}
 
       {isViewModeInProgress && (
         <DatePicker setSelectedDate={handleDateSelection} date={selectedDate} />
@@ -84,8 +113,26 @@ const styles = StyleSheet.create({
   buttonClose: {
     backgroundColor: "#2196F3",
   },
+  actionButton: {
+    borderWidth: 1,
+    borderRadius: 10,
+  },
+  doneButton: {
+    borderColor: "green",
+  },
+  deleteButton: {
+    borderColor: "red",
+  },
   textStyle: {
     color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  actionText: {
+    display: "flex",
+    alignItems: "center",
+    gap: 4,
+    color: "black",
     fontWeight: "bold",
     textAlign: "center",
   },
