d0c2d12c861a2a0296bb10ec645809d476db884e
kelter-js <spawnkillerqw@gmail.com>
2024-07-26 18:24:56 +0500

    feat: implemented task expiration check util function

--- utils/isTaskExpired.ts
+++ utils/isTaskExpired.ts
@@ -1,7 +1,4 @@
-//here we should write logic that will check incoming date with current date, if task time is older than now - return true
-const isTaskExpired = (taskDate: Date): boolean => {
-  const currentDate = new Date();
-  return false;
-};
+const isTaskExpired = (taskDate: Date): boolean =>
+  Boolean(taskDate < new Date());
 
 export default isTaskExpired;
