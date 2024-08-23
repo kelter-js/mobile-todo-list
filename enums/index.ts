e7ffb7be456035e8de85cae03d57521ff44ace69
kelter-js <spawnkillerqw@gmail.com>
2024-08-23 17:55:01 +0500

    feat: move enums into separated file

---
+++ enums/index.ts
@@ -0,0 +1,16 @@
+export enum ViewModes {
+  FINISHED = "FINISHED",
+  IN_PROGRESS = "IN_PROGRESS",
+}
+
+export enum YEAR_SEASONS {
+  WINTER = "WINTER",
+  SPRING = "SPRING",
+  SUMMER = "SUMMER",
+  AUTUMN = "AUTUMN",
+}
+
+export enum ACTIONS {
+  UPDATING = "Updating",
+  LOADING = "Loading",
+}
