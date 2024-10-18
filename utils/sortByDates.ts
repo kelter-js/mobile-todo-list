a5b6b72f71451604be0aa0903493ead610353703
kelter-js <spawnkillerqw@gmail.com>
2024-10-18 17:38:08 +0500

    fix: type safety errors

--- utils/sortByDates.ts
+++ utils/sortByDates.ts
@@ -1,9 +1,16 @@
 import { SORT_DIRECTIONS } from "../entities/SortDirections";
 import { ITask } from "../models";
 
+const IS_EQUAL = 0;
+
 export const sortByDates = (tasks: ITask[], direction: SORT_DIRECTIONS) =>
-  [...tasks].sort(
-    (a, b) =>
-      (direction === SORT_DIRECTIONS.ASC ? b : a)?.triggerDate.getTime() -
-      (direction === SORT_DIRECTIONS.ASC ? a : b)?.triggerDate.getTime()
-  );
+  [...tasks].sort((a, b) => {
+    const firstTask = direction === SORT_DIRECTIONS.ASC ? b : a;
+    const secondTask = direction === SORT_DIRECTIONS.ASC ? a : b;
+
+    if (firstTask.triggerDate && secondTask.triggerDate) {
+      return firstTask.triggerDate.getTime() - secondTask.triggerDate.getTime();
+    }
+
+    return IS_EQUAL;
+  });
